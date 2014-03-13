b530d9d51aa8adda9aef0d278abdb11cb520b927
Kosta Harlan <kostajh@gmail.com>
2014-03-13 14:32:52 -0400

    Building out app

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
 App = Ember.Application.create();
 
+var sites = [{
+    id: '1',
+    title: 'test',
+  }, {
+    id: '2',
+    title: 'two',
+}];
+
 App.Router.map(function() {
-  // put your routes here
+  this.resource('sites', function() {
+    this.resource('site', { path: ':site_id' });
+  });
+  this.resource('about');
 });
 
-App.IndexRoute = Ember.Route.extend({
+App.SitesRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return sites;
   }
+});
+
+App.SiteRoute = Ember.Route.extend({
+  model: function(params) {
+    return sites.findBy('id', params.site_id);
+  }
+});
+
+App.SiteAdapter = DS.RESTAdapter.extend({
+    host: 'http://localhost/'
 });
